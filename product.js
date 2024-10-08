fdee353b5c2328d63281b66ad2adab75f3454e80
Gabriel Felip Mano Lasig <GM887@live.mdx.ac.uk>
2024-10-08 11:53:11 +0100

    moved products data to separate js file, integrated into html

---
+++ product.js
@@ -0,0 +1,13 @@
+let products = [{
+    id: 1001,
+    name: "Test product name",
+    place: "Test location",
+    slots: 3,
+    price: 10,
+}, {
+    id: 1002,
+    name: "Test product name number 2",
+    place: "Test location over there",
+    slots: 3,
+    price: 21,
+}]
