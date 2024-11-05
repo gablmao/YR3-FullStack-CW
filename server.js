064a374f1dd3e8e2ee3dde98e4242aa7e1c43f2c
Gabriel Felip Mano Lasig <88683534+gablmao@users.noreply.github.com>
2024-11-05 13:18:28 +0000

    Update server.js
    developing expressjs, added mongodb-ish

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
+// ----------------------- MONGODB Connection ----------------------- 
+const {
+  MongoClient,
+  ServerApiVersion,
+  ObjectId
+} = require("mongodb");
+const uri = "mongodb+srv://WebstoreAdmin:admin@webstorecluster.si7uv.mongodb.net/?retryWrites=true&w=majority&appName=WebstoreCluster";
+
+const client = new MongoClient(uri, {
+  serverApi: ServerApiVersion.v1
+});
+// ----------------------- MONGODB Connection ----------------------- 
+
+
 //http module is required to create a server
 var http = require("http");
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 
 var app = express();
+
+//logging middleware
+app.use(function(request, response, next) {
+  console.log("New request: ", request.url);
+  next();
+});
+
+//where the html is located
 app.use(express.static('public'));
 
-//handles incoming http request
-function requestHandler(request, response) {
-  response.end("Hello, World!");
-}
 
 
-//app.get("/", function (request, response) {
-  //response.send("Hello, World!");
-//});
 
-//create server that uses function to handle requests
-var server = http.createServer(requestHandler);
-
-//start server to listen to port 3000
-//app.listen();
-server.listen(3000);
+//listen at port 3000
+http.createServer(app).listen(3000, "0.0.0.0", function() {
+  console.log("Server is running at port 3000");
+});
